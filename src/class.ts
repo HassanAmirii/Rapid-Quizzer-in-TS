3fc17b2b7c8cad43dae05ecaa5fb95328d0f0c21
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-07-20 21:31:50 +0100

    refactor: define classGroup interface and sectionData type for JSON structure

--- src/class.ts
+++ src/class.ts
@@ -1,5 +1,16 @@
-interface classData {
+// first; asserting (inner data) class type
+interface classGroup {
   class: string;
   teacher: string;
   students: string[];
+}
+
+// second; let's tell ts the possiblities of our json files
+type sectionData = classGroup | {};
+
+// third; overall/top level class type
+interface fetchJsonData {
+  primarySection: classGroup[];
+  juniorSection: sectionData[];
+  secondarySection: sectionData[];
 }
